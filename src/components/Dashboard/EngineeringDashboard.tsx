50e8f78ae9045b17356952b80f1ef6251a13bed8
Vikrant Shitole <vikrantshitole98@gmail.com>
2025-06-01 11:49:03 +0530

    role based dashboard

---
+++ src/components/Dashboard/EngineeringDashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import type { ChartDataProps } from '../../types/charts'
+import Analytics from '../Analyrics'
+
+const EngineeringDashboard: React.FC<ChartDataProps> = ({ data }) => {
+    return (
+        <div className='card bg-base-100 grid-cols-1 sm:grid-cols-2 shadow-xl shadow-slate-400 dark:shadow-slate-800'>
+            <div className='card-body mx-2 my-2'>
+                <p className='text-gray-200 dark:text-gray-700 font-lg mx-2 my-2 font-semibold text-center'>Engineering Analytics</p>
+            </div>
+            {data && <Analytics data={data} />}
+        </div>
+    )
+}
+
+export default EngineeringDashboard
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Analytics from '../components/Analytics'
 import useChartData from '../hooks/useChartData'
 import FallBack from '../components/FallBack'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../store/authSlice'
+import { lazy } from 'react'
+type RoleName = 'Sales' | 'Engineering' | 'Admin';
 
+const Components: Record<RoleName, React.LazyExoticComponent<React.FC<any>>> = {
+    Sales: lazy(() => import('../components/Dashboard/SalesDashboard')),
+    Engineering: lazy(() => import('../components/Dashboard/EngineeringDashboard')),
+    Admin: lazy(() => import('../components/Dashboard/AdminDashboard'))
+}
 const Dashboard = () => {
     const { data, loading, error } = useChartData()
+    const user = useSelector(selectUser)
+    const roleName = user.role.name as RoleName;
+    const Component = Components[roleName]
     if (loading) {
         return <FallBack />
     }
@@ -12,16 +22,13 @@ const Dashboard = () => {
         throw error
     }
     return (
-        <div className='container my-1'>
-            <div className='grid grid-rows-1 grid-cols-1 gap-4'>
-                <div className='card bg-base-100 grid-cols-1 sm:grid-cols-2 shadow-xl shadow-slate-400 dark:shadow-slate-800'>
-                    <div className='card-body mx-2 my-2'>
-                        <p className='text-gray-200 dark:text-gray-700 font-lg mx-2 my-2 font-semibold text-center'>Analytics</p>
-                    </div>
-                    {data && <Analytics data={data} />}
+        <>
+            <div className='container my-1'>
+                <div className='grid grid-rows-1 grid-cols-1 gap-4'>
+                    <Component data={data}/>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
